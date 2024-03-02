07b79f8279e9175a2eab893012c7a59ff713505b
isaacl81 <isaaclin2007@gmail.com>
2024-03-01 22:40:02 -0500

    added prices

--- vite-project/src/stores/catalog.js
+++ vite-project/src/stores/catalog.js
@@ -1,14 +1,3 @@
-import { defineStore } from "pinia"
-import { ref } from "vue"
-
-// export const useCatalog = defineStore("catalog", () => {
-//     const catalogItems = ref(
-//         [
-//         ]
-//     )
-//     return {catalogItems}
-// })
-
 export const forSale = [
     {
         name: "Amogus Nugget",
@@ -17,32 +6,32 @@ export const forSale = [
     },
     {
         name: "Bidenium",
-        unitCost: 1,
+        unitCost: 1942,
         img: "../public/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.jpg",
     },
     {
         name: "Cinder Block",
-        unitCost: 1,
+        unitCost: 5,
         img: "../public/cinder-blocks-30161345-64_600.jpg",
     },
     {
         name: "Comically Large Spoon",
-        unitCost: 1,
+        unitCost: 2013,
         img: "../public/artworks-36z5mjQj8jbtJrYl-PGkYdw-t500x500.jpg",
     },
     {
         name: "EMD F40PH",
-        unitCost: 1,
+        unitCost: 281,
         img: "../public/AMTK_315_Tun17_CZ_Nwcstle_Mar1995RP_-_Flickr_-_drewj1946.jpg",
     },
     {
         name: "Enslaved Moisture",
-        unitCost: 1,
+        unitCost: 2,
         img: "../public/ah-yes-enslaved-x-enslaved-moisture-hd-templates-1896x1736-v0-0ylpki97jye91.jpg",
     },
     {
         name: "Epic Gaming Chair",
-        unitCost: 1,
+        unitCost: 1337,
         img: "../public/zt1v9a1m.jpg",
     },
     {
@@ -52,7 +41,7 @@ export const forSale = [
     },
     {
         name: "Komatsu 20 BX50 Forklift",
-        unitCost: 1,
+        unitCost: 10000,
         img: "../public/Komatsu_BX50_00.jpg75CA011C-027C-4F09-8909-A19AC8855112Large.jpg",
     },
     {
@@ -62,17 +51,17 @@ export const forSale = [
     },
     {
         name: "🗿",
-        unitCost: 1,
+        unitCost: 13,
         img: "../public/rano-raraku-moai-statues-6.jpg",
     },
     {
         name: "Mystery Box",
-        unitCost: 1,
+        unitCost: 15,
         img: "../public/uvt4t1mrqo161.jpg",
     },
     {
         name: "Nvidia RTX Potato",
-        unitCost: 1,
+        unitCost: 400,
         img: "../public/PtQPU.jpg",
     },
     {
@@ -82,32 +71,32 @@ export const forSale = [
     },
     {
         name: "RIM-116 Rolling Airframe Missile",
-        unitCost: 1,
+        unitCost: 998000,
         img: "../public/USS_New_Orleans_(LPD-18)_launches_RIM-116_missile_2013.jpg",
     },
     {
         name: "Samsung Smart Fridge",
-        unitCost: 1,
+        unitCost: 3500,
         img: "../public/rdokjpvpogpv.jpg",
     },
     {
         name: "Steamed Hams",
-        unitCost: 1,
+        unitCost: 3,
         img: "../public/Screen_Shot_2020_08_18_at_3.45.15_PM.jpg",
     },
     {
         name: "Thermonuclear Warhead",
-        unitCost: 1,
+        unitCost: 85000000,
         img: "../public/Nuclear explosion (Gerd Altmann)_1.jpg",
     },
     {
         name: "Uranus",
-        unitCost: 1,
+        unitCost: 69420,
         img: "../public/PIA18182~orig.jpg",
     },
     {
         name: "2001 Honda Civic",
-        unitCost: 1,
+        unitCost: 1000,
         img: "../public/2001 Honda Civic.jpg",
     },
 ]
